ac669850cfcecea3d95ced6cbc4e25613a07c33b
David Benshachar <dbenshachar@gmail.com>
2025-10-26 12:55:36 -0700

    feat: add cosine similarity function.

--- golang/embedding.go
+++ golang/embedding.go
@@ -4,8 +4,10 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
+	"math"
 	"net/http"
 	"time"
 )
@@ -54,4 +56,23 @@ func GetEmbedding(ctx context.Context, baseURL, model, text string) ([]float32, 
 	}
 
 	return resp.Embedding, nil
+}
+
+func CosineSimilarity(a, b []float64) (float64, error) {
+	if len(a) != len(b) {
+		return 0, errors.New("vectors must be the same size")
+	}
+
+	var dotProduct, magA, magB float64
+	for i := range a {
+		dotProduct += a[i] * b[i]
+		magA += a[i] * a[i]
+		magB += b[i] * b[i]
+	}
+
+	if magA == 0 || magB == 0 {
+		return 0, nil
+	}
+
+	return dotProduct / (math.Sqrt(magA) * math.Sqrt(magB)), nil
 }
--- golang/mongodb.go
+++ golang/mongodb.go
@@ -2,11 +2,24 @@ package golang
 
 import (
 	"context"
+	"os"
 
 	"go.mongodb.org/mongo-driver/v2/mongo"
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
 	"go.mongodb.org/mongo-driver/v2/mongo/readpref"
 )
+
+func GetVector(message string) ([]float32, error) {
+	ctx := context.Background()
+	url := "http://localhost:" + os.Getenv("ollama_host")
+
+	vec, err := GetEmbedding(ctx,
+		url,
+		os.Getenv("ollama_model"),
+		message)
+
+	return vec, err
+}
 
 func MongoClient(mongoURI string) (*mongo.Client, error) {
 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
@@ -22,4 +35,5 @@ func MongoClient(mongoURI string) (*mongo.Client, error) {
 type Document struct {
 	Contents  string    `bson:"contents"`
 	Embedding []float32 `bson:"embedding"`
+	EmailID   string    `bson:"email_id"`
 }
